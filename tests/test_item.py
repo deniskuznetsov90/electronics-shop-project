92b124ca1c277c5a27b05518a3ba3a42fcd4a72d
Denis Kuznetsov <kdv522@mail.ru>
2023-06-26 17:12:00 +0300

    1 commit

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,17 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+from src.item import Item
+
+def test_init():
+    exp = Item("name", 1, 2)
+    assert exp.name == "name"
+    assert exp.price == 1
+    assert exp.quantity == 2
+
+def test_calculate_total_price():
+    exp = Item("name", 1, 2)
+    assert exp.calculate_total_price() == 2
+
+def test_apply_discount():
+    exp = Item("name", 1, 2)
+    assert exp.pay_rate * exp.price == 1
